2a134cd81a0c638039ab26d8be54977ea880e678
sandeep-incedo <iamsandeep344@gmail.com>
2021-10-06 12:44:02 +0530

    Add files via upload

--- src/home/home.js
+++ src/home/home.js
@@ -1,1 +1,16 @@
+import { useSelector } from "react-redux";
+import { useHistory, Link } from 'react-router-dom';
+import './home.css';
 
+export default function HomeComponent (props){
+    const credentials = useSelector(state => state.userCredentials);
+    const history = useHistory();
+    if(!credentials.token)
+        history.push('/');
+    return (
+        <div>
+            <Link to="/createTask"><button type='button'>Create New Task</button></Link>
+            <Link to="/showTasks"><button type='button'>List All Tasks</button></Link>
+        </div>
+    )
+}
