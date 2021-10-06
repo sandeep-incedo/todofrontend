a49d157790edb523f0d70e251d5340c001471f74
sandeep-incedo <iamsandeep344@gmail.com>
2021-10-06 12:45:56 +0530

    Add files via upload

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,15 @@
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+import userCredentialsReducer from "../reducers/userCredentialsReducer";
+import thunk from 'redux-thunk';
 
+const reducers = combineReducers({
+    userCredentials:userCredentialsReducer
+});
+
+const store = createStore(
+    reducers, 
+    {},
+    applyMiddleware(thunk)
+);
+
+export default store;
