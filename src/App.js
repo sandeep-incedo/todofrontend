cfb520dfa0d73c583dab726cd4a0ba161c4c9e26
sandeep-incedo <iamsandeep344@gmail.com>
2021-10-06 12:36:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AllRoutes from './routes/routes';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:'Blue', backgroundColor:'lightgray'}}> TODO App</h1><hr/>
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
